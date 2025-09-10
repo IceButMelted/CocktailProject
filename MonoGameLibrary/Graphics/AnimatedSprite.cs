316e5890d00d08f955d04717fec366f3d1d82e0c
Chatdanai Kantawong <93586231+IceButMelted@users.noreply.github.com>
2025-09-10 19:58:57 +0700

    update AnimatedSpreite

--- MonoGameLibrary/Graphics/AnimatedSprite.cs
+++ MonoGameLibrary/Graphics/AnimatedSprite.cs
@@ -1,130 +1,156 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGameLibrary.Graphics;
 
-namespace MonoGameLibrary.Graphics;
-
-public class AnimatedSprite : Sprite
+namespace MonoGameLibrary.Graphics
 {
-    private int _currentFrame;
-    private TimeSpan _elapsed;
-    private Animation _animation;
-
-    /// <summary>
-    /// Gets or Sets the animation for this animated sprite.
-    /// </summary>
-    public Animation Animation
+    public class AnimatedSprite : Sprite
     {
-        get => _animation;
-        set
+        private int _currentFrame;
+        private TimeSpan _elapsed;
+        private Animation _animation;
+        private bool _isPlaying = true;
+
+        /// <summary>
+        /// Gets or Sets the animation for this animated sprite.
+        /// </summary>
+        public Animation Animation
         {
-            _animation = value;
-            Region = _animation.Frames[0];
+            get => _animation;
+            set => SetAnimation(value, true);
         }
-    }
 
-    /// <summary>
-    /// Creates a new animated sprite.
-    /// </summary>
-    public AnimatedSprite() { }
+        /// <summary>
+        /// Whether the animation is currently playing.
+        /// </summary>
+        public bool IsPlaying => _isPlaying;
 
-    /// <summary>
-    /// Creates a new animated sprite with the specified frames and delay.
-    /// </summary>
-    /// <param name="animation">The animation for this animated sprite.</param>
-    public AnimatedSprite(Animation animation)
-    {
-        Animation = animation;
-    }
+        /// <summary>
+        /// Total number of frames in the animation.
+        /// </summary>
+        public int FrameCount => _animation?.Frames.Count ?? 0;
 
-    /// <summary>
-    /// Updates this animated sprite.
-    /// </summary>
-    /// <param name="gameTime">A snapshot of the game timing values provided by the framework.</param>
-    public void Update(GameTime gameTime)
-    {
-        _elapsed += gameTime.ElapsedGameTime;
+        /// <summary>
+        /// Creates a new animated sprite.
+        /// </summary>
+        public AnimatedSprite() { }
 
-        if (_elapsed >= _animation.Delay)
+        /// <summary>
+        /// Creates a new animated sprite with the specified animation.
+        /// </summary>
+        public AnimatedSprite(Animation animation)
         {
-            _elapsed -= _animation.Delay;
-            _currentFrame++;
+            SetAnimation(animation);
+        }
 
-            if (_currentFrame >= _animation.Frames.Count)
-            {
-                _currentFrame = 0;
-            }
+        // -------------------
+        // CONTROL METHODS
+        // -------------------
 
+        public void Play() => _isPlaying = true;
+        public void Pause() => _isPlaying = false;
+
+        public void Stop()
+        {
+            _isPlaying = false;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _currentFrame = 0;
+            _elapsed = TimeSpan.Zero;
+            if (_animation != null && _animation.Frames.Count > 0)
+                Region = _animation.Frames[0];
+        }
+
+        public void SetAnimation(Animation animation, bool reset = true)
+        {
+            _animation = animation;
+            if (reset) Reset();
+        }
+
+        public void GoToFrame(int frameIndex)
+        {
+            if (_animation == null || _animation.Frames.Count == 0)
+                return;
+
+            _currentFrame = Math.Clamp(frameIndex, 0, _animation.Frames.Count - 1);
             Region = _animation.Frames[_currentFrame];
         }
-    }
 
-    /// <summary>
-    ///  Update with Freez specific frame is true or Loop roll back animation is true.
-    /// </summary>
-    /// <param name="gameTime">A snapshot of the game timing values provided by the framework.</param>
-    public void Update(GameTime gameTime, int SpecificFrame, bool IsFreezeFramem = false, bool IsReverse = false)
-    {
-        _elapsed += gameTime.ElapsedGameTime;
+        public int GetCurrentFrame() => _currentFrame;
 
-        if (_elapsed >= _animation.Delay)
+        // -------------------
+        // UPDATE METHODS
+        // -------------------
+
+        public void Update(GameTime gameTime)
         {
-            _elapsed -= _animation.Delay;
-            if (IsFreezeFramem)
-                _currentFrame = SpecificFrame;
-            else if (IsReverse)
-                _currentFrame--;
-            else
-                _currentFrame++;
+            if (!_isPlaying || _animation == null) return;
 
-            if (_currentFrame >= _animation.Frames.Count)
-            {
-                _currentFrame = 0;
-            }
-            if (_currentFrame < 0)
-            {
-                _currentFrame = _animation.Frames.Count - 1;
-            }
+            _elapsed += gameTime.ElapsedGameTime;
 
-            Region = _animation.Frames[_currentFrame];
+            if (_elapsed >= _animation.Delay)
+            {
+                _elapsed -= _animation.Delay;
+                _currentFrame = (_currentFrame + 1) % _animation.Frames.Count;
+                Region = _animation.Frames[_currentFrame];
+            }
+        }
+
+        public void Update(GameTime gameTime, int SpecificFrame, bool IsFreezeFrame = false, bool IsReverse = false)
+        {
+            if (!_isPlaying || _animation == null) return;
+
+            _elapsed += gameTime.ElapsedGameTime;
+
+            if (_elapsed >= _animation.Delay)
+            {
+                _elapsed -= _animation.Delay;
+
+                if (IsFreezeFrame)
+                    _currentFrame = SpecificFrame;
+                else if (IsReverse)
+                    _currentFrame--;
+                else
+                    _currentFrame++;
+
+                if (_currentFrame >= _animation.Frames.Count) _currentFrame = 0;
+                if (_currentFrame < 0) _currentFrame = _animation.Frames.Count - 1;
+
+                Region = _animation.Frames[_currentFrame];
+            }
+        }
+
+        public void Update(GameTime gameTime, int SpecificFrame, bool IsFreezeFrame = false, bool IsReverse = false, SpriteEffects spriteEffects = SpriteEffects.None)
+        {
+            if (!_isPlaying || _animation == null) return;
+
+            _elapsed += gameTime.ElapsedGameTime;
+
+            if (_elapsed >= _animation.Delay)
+            {
+                _elapsed -= _animation.Delay;
+
+                if (IsFreezeFrame)
+                    _currentFrame = SpecificFrame;
+                else if (IsReverse)
+                    _currentFrame--;
+                else
+                    _currentFrame++;
+
+                if (_currentFrame >= _animation.Frames.Count) _currentFrame = 0;
+                if (_currentFrame < 0) _currentFrame = _animation.Frames.Count - 1;
+
+                Region = _animation.Frames[_currentFrame];
+                Effects = spriteEffects;
+            }
+        }
+
+        public bool IsLastFrame()
+        {
+            return _currentFrame == (_animation?.Frames.Count ?? 1) - 1;
         }
     }
-
-    /// <summary>
-    ///  Update with Freez specific frame is true or Loop roll back animation is true and flip or not.
-    /// </summary>
-    /// <param name="gameTime">A snapshot of the game timing values provided by the framework.</param>
-    public void Update(GameTime gameTime, int SpecificFrame, bool IsFreezeFramem = false, bool IsReverse = false, SpriteEffects spriteEffects = SpriteEffects.None)
-    {
-        _elapsed += gameTime.ElapsedGameTime;
-        if (_elapsed >= _animation.Delay)
-        {
-            _elapsed -= _animation.Delay;
-            if (IsFreezeFramem)
-                _currentFrame = SpecificFrame;
-            else if (IsReverse)
-                _currentFrame--;
-            else
-                _currentFrame++;
-            if (_currentFrame >= _animation.Frames.Count)
-            {
-                _currentFrame = 0;
-            }
-            if (_currentFrame < 0)
-            {
-                _currentFrame = _animation.Frames.Count - 1;
-            }
-            Region = _animation.Frames[_currentFrame];
-            Effects = spriteEffects;
-        }
-    }
-
-
-    public bool IsLastFrame()
-    {
-        return _currentFrame == _animation.Frames.Count - 1;
-    }
-
-
 }
