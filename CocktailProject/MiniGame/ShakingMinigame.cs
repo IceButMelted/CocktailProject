5e3b149285829be5e2cc9785279a325c9e0dffb6
Chatdanai Kantawong <93586231+IceButMelted@users.noreply.github.com>
2025-09-21 18:25:58 +0700

    update MiniGameTesting

---
+++ CocktailProject/MiniGame/ShakingMinigame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MonoGameLibrary;
+using MonoGameLibrary.Input;
+
+namespace CocktailProject.MiniGame
+{
+    static class ShakingMinigame
+    {
+        public static float MaxValue;
+        public static float MinValue;
+
+        public static float TopBound;
+        public static float BottomBound;
+
+        public static float currentVale;
+
+        public static float increaseSpeed;
+        public static float decreaseSpeed;
+
+        public static float cooldownNextClick;
+
+        private static float CurrentCoolDown;
+
+        public static void Update(GameTime gameTime) { 
+            
+        }
+
+        private static void DecreaseValue() {
+            if (CurrentCoolDown > 0) 
+                currentVale -= decreaseSpeed;
+        }
+
+        private static void IncreaseValue() { 
+            if(currentVale < 100)
+                currentVale += increaseSpeed;
+            if (currentVale > 100)
+                currentVale = 100;
+        }
+    }
+}
