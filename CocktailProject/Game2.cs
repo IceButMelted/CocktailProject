90941af298f042b3a72873c08315e25f9a9d1ed7
Chatdanai Kantawong <93586231+IceButMelted@users.noreply.github.com>
2025-10-08 10:31:41 +0700

    update game

--- CocktailProject/Game2.cs
+++ CocktailProject/Game2.cs
@@ -30,7 +30,7 @@ namespace CocktailProject
         {
             base.Initialize();
 
-            ChangeScene(new Scenes.Summary());
+            ChangeScene(new Scenes.GameIntro());
         }
 
         protected override void LoadContent() {
